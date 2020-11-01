b52cbe7093fb7127836c11881febb254e0ea2844
kirill.stakheyko <kirill.stakheyko@gmail.com>
2020-11-01 09:05:48 +0300

    feat: finish last tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	calculateDepth(arr) {
+		if (Array.isArray(arr)) {
+			return 1 + Math.max(...arr.map(nextArr => {
+      if (nextArr.length == 0) 	{
+        return 1;
+      } else
+				return this.calculateDepth(nextArr);
+			}));
+		} else {
+			return 0;
+		}
+	}
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,65 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    this.value = value;
+  }
+
+  encrypt(message, key) {
+    if (message !== undefined && key !==undefined) {
+      let count = 0;
+      let reg = /[a-zA-Z]/,
+      resultArr = [],
+      messageLow = message.toLowerCase(),
+      keyLow = key.toLowerCase();
+      for (let i = 0; i < message.length; i++) {
+        if (reg.test(message[i])) {
+          let code = keyLow.charCodeAt(count);
+          resultArr.push( String.fromCharCode(
+              ((messageLow.charCodeAt(i) - 97 + (code - 97)) % 26) + 97)
+          );
+          count++;
+          if (count == key.length) {
+            count = 0; 
+          }
+        } else resultArr.push(message[i]);
+      }
+      return this.value === false ? resultArr.reverse().join("").toUpperCase(): resultArr.join("").toUpperCase();
+    }
+    throw new Error("erroe");
+  }
+
+  decrypt(message, key) {
+
+    if (message !== undefined && key !== undefined) {
+      let count = 0;
+      let reg = /[a-zA-Z]/,
+      resultArr = [],
+      messageLow = message.toLowerCase(),
+      keyLow = key.toLowerCase();
+      for (let i = 0; i < message.length; i++) {
+        if (reg.test(message[i])) {
+          let code = keyLow.charCodeAt(count);
+          resultArr.push(
+            String.fromCharCode(
+              ((messageLow.charCodeAt(i) - 97 - (code - 97) + 26) % 26) + 97
+            )
+          );
+          count++;
+          if (count === key.length) {
+            count = 0;
+
+
+          }
+        } 
+      else {
+        resultArr.push(message[i]);
+      }
+      }
+
+      return this.value == false ? resultArr.reverse().join("").toUpperCase() : resultArr.join("").toUpperCase();
+    }
+    throw new Error("error");
   }
 }
-
 module.exports = VigenereCipheringMachine;
